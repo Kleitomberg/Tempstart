e5e855a743ad88c61aec052609e6c673a3ded2ec
kleitomberg <kleitom-1@hotmail.com>
2022-10-25 19:32:51 -0300

    ajuste to mobile

--- main.js
+++ main.js
@@ -7,32 +7,56 @@ const now_temp = document.querySelector('.now .temperatura')
 const max_temp = document.querySelector('.max .temperatura')
 const min_temp = document.querySelector('.min .temperatura')
 
+const iconNowTemp = document.querySelector('.now img')
+
+const img = document.querySelector('.loading')
+
 input_city.addEventListener('change', (e) => {
   e.preventDefault()
   getTemperatura(input_city.value)
 })
 
-function getTemperatura(city) {
+function setIcon(descricao) {
+  const desc = String(descricao).replace(' ', '_')
+  const icons = {
+    nublado: 'cloud',
+    chuva_moderada: 'rain',
+    chuva_leve: 'rain',
+    algumas_nuvens: 'cloud',
+    nuvens_dispersas: 'cloud',
+    céu_limpo: 'sun',
+    default: 'sun',
+  }
+  console.log(desc)
+  console.log(iconNowTemp)
+
+  iconNowTemp.setAttribute('src', `${icons[desc]}.svg`)
+}
+
+async function getTemperatura(city) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},br&lang=pt_br&appid=96120036ea8d15f72dad65c4064684bd`
 
-  fetch(url)
+  await fetch(url)
     .then(function (response) {
-      response.json().then(function (data) {
-        console.log(data)
+      response.json().then((data) => {
         if (data.cod == '404') {
           alert(`Error: Nenhuma Cidade encontrada como ${city}!`)
         } else {
           city_name.innerText = data.name
+          //convertendo em celsius
           const temperatura = data.main.temp - 273.15
           const temperatura_max = data.main.temp_max - 273.15
           const temperatura_min = data.main.temp_min - 273.15
+          //inserindo na tela
           now_temp.innerText = Math.round(temperatura)
           max_temp.innerText = Math.round(temperatura_max)
           min_temp.innerText = Math.round(temperatura_min)
+          console.log(data.weather[0].description)
+          setIcon(data.weather[0].description)
         }
       })
     })
-    .catch(function (err) {
+    .catch((err) => {
       console.error('Failed retrieving information', err)
     })
 }
